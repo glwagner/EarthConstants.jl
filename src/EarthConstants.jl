2e0088d11bd224e1c6430876398504d9805d373c
Gregory Wagner <wagner.greg@gmail.com>
2018-11-09 11:50:25 -0500

    Adds some rotation rate constants and distance constants

--- src/EarthConstants.jl
+++ src/EarthConstants.jl
@@ -7,15 +7,37 @@ export
   day,
   week,
   year,
-  g
+  g,
 
+  stellaryear,
+  Omega,
+  Ω,
+
+  m,
+  μm,
+  mm,
+  km
+
+# Gravitational constants
 const g = 9.81
 
+# Distance
+const m = 1.0
+const μm = 1e-6*m
+const mm = 0.001m
+const km = 1000m
+
+# Gregorian calendar
 const second = 1.0
 const minute = 60second
 const hour = 60minute
 const day = 24hour
 const week = 7day
 const year = 365day # not exactly one year
+
+# Rotation rate
+const stellaryear = 23hour + 56minute + 4.098903691
+const Omega = 2π/stellaryear
+const Ω = Omega
 
 end # module
